76c9726d68fc358906d1e454cabd51c40f210af7
X00188225 <x00188225@mytudublin.ie>
2025-10-02 13:27:46 +0100

    Add greet function

--- GitBasicsLab.py
+++ GitBasicsLab.py
@@ -2,3 +2,8 @@ print("Hello, Git!")
 
 print("Hello, Git!")
 print("Learning about git diff.")
+
+def greet(name):
+    return f"Hello, {name}! Welcome to Git and GitHub."
+
+print(greet("Student"))
